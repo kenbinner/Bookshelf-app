eab2fd715a6e9bce9828d977b81abc2876731494
Kengo <kengo@binner.org>
2020-02-29 20:16:11 +0000

    First commit

---
+++ BookshelfApp-Server/src/test/java/com/kengo/BookshelfAppServer/BookshelfAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kengo.BookshelfAppServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
