6ef6f20775d02f661444d6c8471c3800fbf0d7fa
Kengo <kengo@binner.org>
2020-06-23 12:06:01 +0100

    Reloaded app files

--- BookshelfApp-Client/src/app/app-routing.module.ts
+++ BookshelfApp-Client/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShelfComponent } from './shelf/shelf.component';
+import { AddBookComponent} from './shelf/add-book/add-book.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component:  HomeComponent},
+  {path: 'shelf', component: ShelfComponent},
+  {path: 'add-book', component: AddBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BookshelfApp-Client/src/app/app.module.ts
+++ BookshelfApp-Client/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShelfComponent } from './shelf/shelf.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AddBookComponent } from './shelf/add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShelfComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
