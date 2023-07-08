56e6cd6917e3f19056dbe7956a9de6ac333a39af
kenbinner <kengo@binner.org>
2023-07-08 19:33:43 +0100

    servlet-api fix

--- BookshelfApp-Server/src/main/java/com/kengo/BookshelfAppServer.java
+++ BookshelfApp-Server/src/main/java/com/kengo/BookshelfAppServer.java
@@ -30,12 +30,12 @@ public class BookshelfAppServer{// implements CommandLineRunner{
 	private static final Logger logger = LogManager.getLogger(BookshelfAppServer.class);
 	
 	public static void main(String[] args) {
-		logger.trace("We've just greeted the user!");
-		logger.debug("We've just greeted the user!");
-		logger.info("We've just greeted the user!");
-		logger.warn("We've just greeted the user!");
-		logger.error("We've just greeted the user!");
-		logger.fatal("We've just greeted the user!");
+//		logger.trace("We've just greeted the user!");
+//		logger.debug("We've just greeted the user!");
+//		logger.info("We've just greeted the user!");
+//		logger.warn("We've just greeted the user!");
+//		logger.error("We've just greeted the user!");
+//		logger.fatal("We've just greeted the user!");
 		SpringApplication.run(BookshelfAppServer.class, args);
 	}
 	
--- BookshelfApp-Server/src/main/java/com/kengo/service/BookshelfService.java
+++ BookshelfApp-Server/src/main/java/com/kengo/service/BookshelfService.java
@@ -1,4 +1,4 @@
-package com.kengo.api;
+package com.kengo.service;
 
 import java.util.List;
 
